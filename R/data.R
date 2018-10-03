431b6affa736a033426e0eb40665b4e0aa9c5f79
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-10-03 16:24:15 +0300

    added glasser

--- R/data.R
+++ R/data.R
@@ -127,8 +127,7 @@
 #' @keywords datasets
 #'
 #' @references Glasser et al. (2016) Nature, volume 536, pages 171–178
-#' (\href{https://www.nature.com/articles/nature18933}{PubMed}) &
-#' (\href{https://figshare.com/articles/HCP-MMP1_0_projected_on_fsaverage/3498446})
+#' (\href{https://www.nature.com/articles/nature18933}{PubMed})
 #'
 #' @format A data.frame with 2483 observations and 8 variables
 #' \describe{
@@ -137,11 +136,33 @@
 #'   \item{area}{name of region}
 #'   \item{hemi}{dummy name of the hemisphere}
 #'   \item{side}{which side to view (medial, lateral)}
+#'   \item{label}{label name from Freesurfer}
 #' }
 #'
 #' @examples
 #' data(glasser)
 "glasser"
+
+#' Parcellation from the Human Connectome Project
+#'
+#' @docType data
+#' @name midsagittal
+#' @keywords datasets
+#'
+#'
+#' @format A data.frame with 2483 observations and 8 variables
+#' \describe{
+#'   \item{long}{coordinates for the x-axis}
+#'   \item{lat}{coordinates for the y-axis}
+#'   \item{area}{name of region}
+#'   \item{hemi}{dummy name of the hemisphere}
+#'   \item{side}{which side to view (sagittal)}
+#'   \item{label}{label name from Freesurfer}
+#' }
+#'
+#' @examples
+#' data(midsagittal)
+"midsagittal"
 
 
 #' ggseg palettes
