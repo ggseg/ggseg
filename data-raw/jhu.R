93c101be2a66ded773536ca18d4781129f66a424
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-10-17 21:50:18 +0200

    minor fix in jhu atlas segments

--- data-raw/jhu.R
+++ data-raw/jhu.R
@@ -1,20 +1,22 @@
 jhu = geobrain_JHU %>%
+  mutate(aparc = gsub("ifl", "ilf", aparc)) %>%
   separate(aparc, c("hemi","acronym"), remove = FALSE) %>%
   mutate(acronym = ifelse(!hemi %in% c("lh","rh"), hemi, acronym),
          acronym = ifelse(acronym %in% "gm", NA, acronym),
-         hemi = ifelse(hemi %in% "lh", "left", ifelse(hemi %in% "rh", "right", "center")),
+         aparc = ifelse(aparc %in% "gm", NA, aparc),
+         hemi = ifelse(hemi %in% "lh", "left",
+                       ifelse(hemi %in% "rh", "right", "center")),
   ) %>%
   mutate(side = ifelse(id %in% c(200:240), "upper coronal",
-                       ifelse(id %in% c(1:13), "lower coronal","axial")
-  ),
-  atlas = "jhu"
+                       ifelse(id %in% c(1:13), "lower coronal","axial")),
+         atlas = "jhu"
   )
 
 tmp = data.frame(area = c("Anterior thalamic radiation", "Corticospinal tract","Cingulum (cingulate gyrus)",
-                    "Cingulum (hippocampus)","Forceps major","Forceps minor",
-                    "Inferior fronto-occipital fasciculus","Inferior longitudinal fasciculus","Superior longitudinal fasciculus",
-                    "Uncinate fasciculus","Superior longitudinal fasciculus (temporal part)","Cerebral spinal fluid"),
-           acronym=c("atr","cst","ccg","cab","fmajor","fminor","ifof","ilf","slf","unc","slfp", "csf"))
+                          "Cingulum (hippocampus)","Forceps major","Forceps minor",
+                          "Inferior fronto-occipital fasciculus","Inferior longitudinal fasciculus","Superior longitudinal fasciculus",
+                          "Uncinate fasciculus","Superior longitudinal fasciculus (temporal part)","Cerebral spinal fluid"),
+                 acronym=c("atr","cst","ccg","cab","fmajor","fminor","ifof","ilf","slf","unc","slft", "csf"))
 
 jhu = jhu %>%
   left_join(tmp) %>%
