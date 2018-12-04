e6394b42ca7a5d047f3ca40ab4863d6b95ed265e
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-12-04 13:51:21 +0100

    bug fix in paletteer, added test for it

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -122,6 +122,7 @@ test_that("Check that get_paletteer is working", {
   expect_equal(length(get_paletteer("oslo")),5)
   expect_equal(length(get_paletteer("PonyoMedium")),7)
   expect_equal(length(get_paletteer("seadra")),10)
+  expect_equal(length(get_paletteer("qualitative")),12)
   expect_error(get_paletteer("mononoke"))
 })
 
