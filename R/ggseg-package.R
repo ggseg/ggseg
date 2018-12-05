31b98cdd77a53f1cb4b6646bb420f5bc0b94d852
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-12-05 07:35:03 +0100

    fixed problem with vignette

---
+++ R/ggseg-package.R
@@ -0,0 +1,15 @@
+#' ggseg: Plot brain segmentations with ggplot and plotly
+#'
+#' Plotting results from analyses done on data derived from brain
+#' segmentations is a common need, but may be quite laborious.
+#' Results from such analyses are usually easier to interpret if
+#' the plot can mimmick the shape and position in the brain it
+#' represents.
+#' This package contains data from various brain parcellations,
+#' with convenient functions to inspect the results directly on
+#' a brain-plot wither with ggplot (2D polygons) or plotly (3d meshes).
+#'
+#' @name ggseg
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
