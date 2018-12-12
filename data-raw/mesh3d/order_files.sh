c52c75c6dcc3ff581583189f6532c39d30134171
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-12-12 09:21:54 +0100

    updated readme with Lifebrain funding

--- data-raw/mesh3d/order_files.sh
+++ data-raw/mesh3d/order_files.sh
@@ -12,16 +12,8 @@ for i in $(ls *${1}*zip); do
 done
 
 # Nest by surface
-# for i in $(ls -d [rl]*${1}*inflated/*/); do 
-# 	echo $i
-# 	new=$(echo $i | cut -d"/" -f1 | sed -e s/.inflated//g)/inflated)
-# 	mkdir -p $new; 
-# 	mv $i/* $new; 
-# 	rm -rf $i
-# done
-
-for surf in white inflated; do
-	for i in $(ls -d [rl]*${1}*${surf}/*/); do 
+for surf in white inflated LCBC; do
+	for i in $(ls -d [rl]*${1}*${surf}/); do 
 		echo $i
 		new=$(echo $i | cut -d"/" -f1 | sed -e s/.${surf}//g)/${surf}
 		mkdir -p $new; 
@@ -46,18 +38,23 @@ for i in $(ls -d *${1}*/); do
 	mkdir -p $i/white/lh;
 	mkdir -p $i/inflated/rh; 
 	mkdir -p $i/inflated/lh; 
+	mkdir -p $i/LCBC/rh; 
+	mkdir -p $i/LCBC/lh; 
 
 	mv $i/rh/white/* $i/white/rh; 
 	mv $i/lh/white/* $i/white/lh; 
 	mv $i/lh/inflated/* $i/inflated/lh; 
 	mv $i/rh/inflated/* $i/inflated/rh; 
+	mv $i/lh/LCBC/* $i/LCBC/lh; 
+	mv $i/rh/LCBC/* $i/LCBC/rh; 
 
 	rm -rf $i/rh/
 	rm -rf $i/lh/
 done
 
-# Remove puncts from foldernames for easier import to R
-for i in $(ls -d *${1}*/); do 
-	mv $i $(echo $i | sed -e s/[[:punct:]]//g); 
-done
+# # Remove puncts from foldernames for easier import to R
+# for i in $(ls -d *${1}*/); do 
+# 	mkdir -p $(echo $i | sed -e s/[[:punct:]]//g)
+# 	mv $i/* $(echo $i | sed -e s/[[:punct:]]//g); 
+# done
 
