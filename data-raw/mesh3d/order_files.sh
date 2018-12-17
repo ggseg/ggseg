223110f73743ab0cdf0c849942aa00e1c642e4e6
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2018-12-17 11:49:59 +0100

    added atlases with LCBC surfaces

--- data-raw/mesh3d/order_files.sh
+++ data-raw/mesh3d/order_files.sh
@@ -13,8 +13,9 @@ done
 
 # Nest by surface
 for surf in white inflated LCBC; do
+	echo $surf 
 	for i in $(ls -d [rl]*${1}*${surf}/); do 
-		echo $i
+		echo .......$i
 		new=$(echo $i | cut -d"/" -f1 | sed -e s/.${surf}//g)/${surf}
 		mkdir -p $new; 
 		mv $i/* $new; 
@@ -25,36 +26,53 @@ done
 
 # Nest by hemisphere
 for hemi in rh lh; do
-	for i in $(ls -d ${hemi}*${1}**/); do 
+	for i in $(ls -d ${hemi}*${1}*/*/); do 
 		mkdir -p  $(echo $i | sed -e s/${hemi}.//g)/${hemi}; 
 		mv $i/* $(echo $i | sed -e s/${hemi}.//g)/${hemi}; 
-		rm -rf $i
+		rm -rf $(echo $i | cut -d/ -f1)
 	done
 done
 
 # Reorganise the folders
-for i in $(ls -d *${1}*/); do 
-	mkdir -p $i/white/rh; 
-	mkdir -p $i/white/lh;
-	mkdir -p $i/inflated/rh; 
-	mkdir -p $i/inflated/lh; 
-	mkdir -p $i/LCBC/rh; 
-	mkdir -p $i/LCBC/lh; 
-
-	mv $i/rh/white/* $i/white/rh; 
-	mv $i/lh/white/* $i/white/lh; 
-	mv $i/lh/inflated/* $i/inflated/lh; 
-	mv $i/rh/inflated/* $i/inflated/rh; 
-	mv $i/lh/LCBC/* $i/LCBC/lh; 
-	mv $i/rh/LCBC/* $i/LCBC/rh; 
-
-	rm -rf $i/rh/
-	rm -rf $i/lh/
-done
-
-# # Remove puncts from foldernames for easier import to R
 # for i in $(ls -d *${1}*/); do 
-# 	mkdir -p $(echo $i | sed -e s/[[:punct:]]//g)
-# 	mv $i/* $(echo $i | sed -e s/[[:punct:]]//g); 
+# 	for surf in $(ls -d $i/*/)
+# 	mkdir -p $i/white/rh; 
+# 	mkdir -p $i/white/lh;
+# 	mkdir -p $i/inflated/rh; 
+# 	mkdir -p $i/inflated/lh; 
+# 	mkdir -p $i/LCBC/rh; 
+# 	mkdir -p $i/LCBC/lh; 
+
+# 	mv $i/rh/white/* $i/white/rh; 
+# 	mv $i/lh/white/* $i/white/lh; 
+# 	mv $i/lh/inflated/* $i/inflated/lh; 
+# 	mv $i/rh/inflated/* $i/inflated/rh; 
+# 	mv $i/lh/LCBC/* $i/LCBC/lh; 
+# 	mv $i/rh/LCBC/* $i/LCBC/rh; 
+
+# 	rm -rf $i/rh/
+# 	rm -rf $i/lh/
 # done
+
+# Remove puncts from foldernames for easier import to R
+for i in $(ls -d *${1}*/); do 
+	NEW=$(echo $i | sed -e s/[[:punct:]]//g)
+
+ 	echo $i $NEW
+	if [ -d $NEW ]; then
+		
+		if [[ ! $i = ${NEW}/ ]]; then
+			for surf in $(ls $i/); do
+				rm -r $NEW/$surf
+				mv $i/$surf $NEW/
+			done
+
+			rmdir $i
+		fi
+
+	else
+		mkdir -p $(echo $i | sed -e s/[[:punct:]]//g)
+		mv $i/* $(echo $i | sed -e s/[[:punct:]]//g); 
+	fi
+done
 
