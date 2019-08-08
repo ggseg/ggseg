b5c482c141d84c4991536698257863209286c120
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2019-08-08 07:38:46 +0200

    added export to install_atlases

--- R/geom-brain.R
+++ R/geom-brain.R
@@ -1,77 +1,77 @@
-#' #' GeomAtlas <- ggproto("GeomPolygon", Geom,
-#' #'                      required_aes = c("atlas"),
-#' #'
-#' #'                      default_aes = aes(
-#' #'                        colour = NA, fill = "grey20", size = 0.5,
-#' #'                        linetype = 1, alpha = 1,
-#' #'                        position = "stacked", view = NULL,
-#' #'                        hemisphere = NULL,
-#' #'                      ),
-#' #'
-#' #'                      draw_key = draw_key_polygon,
-#' #'
-#' #'                      draw_group = function(data, panel_params, coord, atlas) {
-#' #'                        n <- nrow(data)
-#' #'                        if (n <= 2) return(grid::nullGrob())
-#' #'
-#' #'                        data2 <- data_merge(data, atlas)
-#' #'                        cat(names(data2))
-#' #'                        coords <- coord$transform(data2, panel_params)
-#' #'
-#' #'                        # A polygon can only have a single colour, fill, etc, so take from first row
-#' #'                        first_row <- coords[1, , drop = FALSE]
-#' #'
-#' #'                        grid::polygonGrob(
-#' #'                          coords$lat, coords$long,
-#' #'                          default.units = "native",
-#' #'                          gp = grid::gpar(
-#' #'                            col = first_row$colour,
-#' #'                            fill = scales::alpha(first_row$fill, first_row$alpha),
-#' #'                            lwd = first_row$size * .pt,
-#' #'                            lty = first_row$linetype
-#' #'                          )
-#' #'                        )
-#' #'                      }
-#' #' )
-#' #'
-#' #' geom_atlas <- function(mapping = NULL, data = NULL, stat = "identity", atlas = NA,
-#' #'                        position = "identity", na.rm = FALSE, show.legend = NA,
-#' #'                        inherit.aes = TRUE, ...) {
-#' #'   layer(
-#' #'     geom = GeomAtlas, mapping = mapping, data = data, stat = stat,
-#' #'     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-#' #'     params = list(na.rm = na.rm, atlas = atlas, ...)
-#' #'   )
-#' #' }
-#' #'
-#' #'
+# GeomBrain <- ggproto("GeomBrain", Geom,
+#                      required_aes = c("atlas"),
+#
+#                      default_aes = aes(
+#                        colour = NA, fill = "grey20", size = 0.5,
+#                        linetype = 1, alpha = 1,
+#                        position = "stacked", view = NULL,
+#                        hemisphere = NULL,
+#                      ),
+#
+#                      draw_key = draw_key_polygon,
+#
+#                      draw_group = function(data, panel_params, coord, atlas) {
+#                        n <- nrow(data)
+#                        if (n <= 2) return(grid::nullGrob())
+#
+#                        data2 <- data_merge(data, atlas)
+#                        cat(names(data2))
+#                        coords <- coord$transform(data2, panel_params)
+#
+#                        # A polygon can only have a single colour, fill, etc, so take from first row
+#                        first_row <- coords[1, , drop = FALSE]
+#
+#                        grid::polygonGrob(
+#                          coords$lat, coords$long,
+#                          default.units = "native",
+#                          gp = grid::gpar(
+#                            col = first_row$colour,
+#                            fill = scales::alpha(first_row$fill, first_row$alpha),
+#                            lwd = first_row$size * .pt,
+#                            lty = first_row$linetype
+#                          )
+#                        )
+#                      }
+# )
+#
+# geom_brain <- function(mapping = NULL, data = NULL, stat = "identity", atlas = dkt,
+#                        position = "identity", na.rm = FALSE, show.legend = NA,
+#                        inherit.aes = TRUE, ...) {
+#   layer(
+#     geom = GeomBrain, mapping = mapping, data = data, stat = stat,
+#     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+#     params = list(na.rm = na.rm, atlas = atlas, ...)
+#   )
+# }
+
+#'
 #' #' @rdname ggplot2-ggproto
 #' #' @format NULL
 #' #' @usage NULL
 #' #' @export
-#' StatAtlas <- ggproto(
-#'   "StatAtlas",
+#' StatBrain <- ggproto(
+#'   "StatBrain",
 #'   Stat,
 #'   required_aes = c("atlas"),
 #'   default_aes = aes(
 #'     x = stat(x),
 #'     y = stat(y),
-#'     group = stat(id)
+#'     group = stat(id),
+#'     fill = stat(area)
 #'   ),
 #'
-#'   compute_group = function(data, scales, params, atlas) {
-#'     # tmp <- unnest(atlas, ggseg)
-#'     # data2 <- data_merge(data, tmp)
-#'     data2 <- unnest(atlas, ggseg)
+#'   compute_panel = function(data, scales, params, atlas) {
+#'     data2 <- tidyr::unnest(atlas, ggseg)
 #'
 #'     data.frame(x = data2$.long,
 #'                y = data2$.lat,
-#'                id = data2$.id)
+#'                id = data2$.id,
+#'                area = data2$area)
 #'   }
 #' )
-#' #'
-#' #'
-#' stat_atlas <- function(mapping = NULL,
+#'
+#'
+#' stat_brain <- function(mapping = NULL,
 #'                        data = NULL,
 #'                        geom = "polygon",
 #'                        position = "identity",
@@ -80,7 +80,7 @@
 #'                        inherit.aes = TRUE,
 #'                        atlas = NULL,
 #'                        ...) {
-#'   layer(stat = StatAtlas,
+#'   layer(stat = StatBrain,
 #'         data = data,
 #'         mapping = mapping,
 #'         geom = geom,
