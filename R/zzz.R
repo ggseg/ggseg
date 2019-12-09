a8568e5178b4ffc449cf0829d0f303aeb4f06d9e
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2019-12-09 13:36:16 +0100

    merged aseg with midsagittal

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive()) {
-    packageStartupMessage(paste("ggseg by default only loads atlases dkt and aseg in 2 and 3D.",
+    packageStartupMessage(paste("ggseg by default only loads atlases dkt and aseg.",
                                 "To install the remaining atlases, use 'ggseg::install_atlases()'"))
   }
 }
