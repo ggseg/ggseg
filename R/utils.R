dda4064782d05e8c71586a0a868554a919580854
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-26 16:46:49 +0100

    cleanup

--- R/utils.R
+++ R/utils.R
@@ -135,20 +135,6 @@ adapt_scales = function(geobrain, position = "dispersed", aesthetics = "labs"){
   }
 }
 
-
-#' Convenience function to install all atlases from ggsegExtra-repo
-#'
-#' \code{install_atlases} calls devtools::install_github() with repo = "LCBC-UiO/ggsegExtra"
-#' @author Athanasia Mowinckel and Didac Pineiro
-#'
-#' @param repo repository to install via github
-#' @param ... other options to install_github
-#' @export
-install_atlases = function(repo="LCBC-UiO/ggsegExtra",...){
-  remotes::install_github(repo=repo, ...)
-}
-
-
 gap <- function(x){
   (min(x) + max(x)) / 2
 }
