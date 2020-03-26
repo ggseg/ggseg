9d2d88246f235bef038dece125d4016ec117aab7
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2020-03-26 16:46:49 +0100

    cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   if (interactive()) {
     packageStartupMessage(paste("ggseg by default only loads atlases dkt and aseg.",
-                                "To install the remaining atlases, use 'ggseg::install_atlases()'"))
+                                "To install other available atlases, check out LCBC-UiO/ggsegExtra'"))
   }
 }
 
